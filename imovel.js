d2bee686765dd61bece8080723dabe483daf4116
Gabriel Henrique Vaz <58632934+ghvaz@users.noreply.github.com>
2024-08-07 21:40:54 -0300

    Update imovel.js,

--- imovel.js
+++ imovel.js
@@ -4,10 +4,14 @@
 //     bairro 
 //     id_corretora: relacionar com corretora
 const prompt = require("prompt-sync")({sigint:true})
-let ultID = 0 
-let corretoras = require("../index.js")
+let ultID = 3
+let crtrs = require("./corretora.js")
 let crud = require("./cliente.js")
-let a=[]
+let imoveis=[
+{id: 1, bairro: "boa vista", rua: "antonio saad", numero: 2500, corretora: 1},
+{id: 2, bairro: "boa vista", rua: "antonio saad", numero: 2510, corretora: 2},
+{id: 3, bairro: "cará - cará uvaranas", rua: "jose sebastião perreira", numero: 99, corretora: 1},
+]
 
 const imovel = {
     model: (id = ++ultID) => {
@@ -20,7 +24,7 @@ const imovel = {
             return undefined;
         }
         let corretora = (prompt("Digite o corretora da casa: ")).toLocaleLowerCase()
-        let indice = corretoras.findIndex(el => el.nome == corretora)
+        let indice = crtrs.corretoras.findIndex(el => el.nome == corretora)
         if(indice == (-1)){
             console.log("Corretora invalida.");
             
@@ -30,19 +34,37 @@ const imovel = {
         return {id, bairro, rua, numero, corretora}
     },
 
-    add: (array) => {
+    retPorId: (ql) => {
+        let casas = imoveis.filter((imovel) => imovel.id == ql)
+        return casas
+    },
+
+    retPorRua: (ql) => {
+        let casas = imoveis.filter((imovel) => imovel.rua == ql)
+        return casas
+    },
+
+
+    listPorBairro: (ql) => {
+        let casas = imoveis.filter((imovel) => imovel.bairro == ql)
+        return casas
+
+    },
+
+    add: () => {
         let novo = imovel.model()
         if(novo){
-            array.push(novo)
+            imoveis.push(novo)
         }
     },
 
-    list: (array) => {
-        if(array.length != 0){
-            array.forEach(el => {
+    list: () => {
+        if(imoveis.length != 0){
+            imoveis.forEach(el => {
                 console.log("----------------------------")
                 console.log(`ID: ${el.id}
-            End: ${el.rua}, ${el.numero}, ${el.bairro}`);
+        Crrt: ${(crtrs.mostrarCorretora(el.corretora))}
+        End: ${el.rua}, ${el.numero}, ${el.bairro}`);
             });
             console.log("----------------------------")
 
@@ -53,17 +75,69 @@ const imovel = {
             return false
         }
     },
-    remove: (array) => {
-        if(imovel.list(array)){
 
+    remove: () => {
+        if(imovel.list(imoveis)){
+            let ql = +("Qual ID quer excluir: ")
+            imoveis.forEach((el, i) => {
+                if(el.id == ql){
+                    imoveis.splice(i,1)
+                    prompt("Deletado com sucesso. Enter para retornar.")
+                }
+            });
         }
     },
+
+    update: () => {
+        if(imovel.list()){
+            let ql = +prompt("Qual ID quer alterar: ")
+                imoveis.forEach((el, i) => {
+                    if (el.id == ql) {
+                    console.clear
+                    console.log(`ID: ${el.id}
+            Corretora: ${crtrs.retNOME(el.corretora)}
+            End: ${el.rua}, ${el.numero}, ${el.bairro}`);
+                    console.log(`O quer alterar: 
+    1. Bairro
+    2. Rua
+    3. Numero
+    4. corretora`);
+                        let op = +prompt()
+                        switch (op) {
+                            case 1:
+                                let novob = prompt("Digite o novo bairro: ")
+                                el.bairro = novob
+                                break;
+                            case 2:
+                                let novor = prompt("Digite o novo rua: ")
+                                el.rua =novor
+                                
+                                break;
+                            case 3:
+                                let novon = prompt("Digite o novo numero: ")
+                                el.numero =novon
+                                
+                                break;
+                            case 4:
+                                if(crtrs.list()){
+                                    let novoc = +prompt("Digite o ID da nova corretora: ")
+                                    el.corretora = novoc
+                                }else{
+                                    console.log("Sem registro de corretoras.");
+                                }
+
+                                break;
+                        
+                            default:
+                                console.log('erro');
+                                break;
+                        }
+                        prompt("Alterado com sucesso. Enter para retornar.")
+                    }
+                });
+            }
+        },
 }
 
-// imovel.remove(a)
 
-
-// imovel.add(a)
-// imovel.add(a)
-
-// imovel.remove(a)
+module.exports = imovel
